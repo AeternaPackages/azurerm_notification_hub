236815cfcd1717fd5d25cf39b2b6c2858ee7447f
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:41:24 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -47,9 +47,9 @@ EOT
       name                = string
       namespace_name      = string
       resource_group_name = string
-      listen              = optional(bool) # Default: false
-      manage              = optional(bool) # Default: false
-      send                = optional(bool) # Default: false
+      listen              = optional(bool)
+      manage              = optional(bool)
+      send                = optional(bool)
     })))
   }))
 
